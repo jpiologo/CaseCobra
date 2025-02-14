87bd26c137a9ce2dc62b6a1c68ef12312c8bd3a4
Joao Piologo <joaotpiologo@hotmail.com>
2025-02-14 00:03:47 -0300

    patch styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
                 <img src='/snake-1.png' alt="A Smiling Snake" className="w-full"/>
               </div>
               <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl">
-                Your Image on a <span className="bg-green-600 px-2 text-white">Custom</span>Phone Case
+                Your Image on a <span className="bg-green-600 rounded-sm px-2 text-white">Custom</span>Phone Case
               </h1>
               <p className="mt-8 text-lg lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap">
                 Capture your favorite memories with your own, <span className="font-semibold">one-of-one</span> phone case. CaseCobra allows you to protect your memories, not just your phone case.
