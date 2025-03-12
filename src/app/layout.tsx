96af4aaf938766bd99b2055048747d16649c768a
Joao Piologo <joaotpiologo@hotmail.com>
2025-03-11 23:26:32 -0300

    Feat: Drag and Drop Page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,8 +28,13 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Navbar />
-        {children}
-        <Footer />
+
+        <main className="flex flex-col min-h-[calc(100vh-3.5rem-1px)]">
+          <div className="flex-1 flex flex-col h-full">
+            {children}
+          </div>
+          <Footer />
+        </main>
       </body>
     </html>
   );
