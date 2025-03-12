1bbc88fcd6a41e5aa0c163e61ee985e295f828a4
Joao Piologo <joaotpiologo@hotmail.com>
2025-03-12 02:52:58 -0300

    patch: add steps to upload

--- src/app/configure/layout.tsx
+++ src/app/configure/layout.tsx
@@ -1,9 +1,11 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import Steps from "@/components/Steps";
 import type { ReactNode } from "react";
 
 const Layout = ({children}: {children: ReactNode}) => {
     return (
         <MaxWidthWrapper className="flex-1 flex flex-col">
+            <Steps />
             {children}
         </MaxWidthWrapper>
     )
