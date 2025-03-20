18bd988c9b984d303134c2593d46d048af04ddc0
João Piologo <joaotpiologo@hotmail.com>
2025-03-19 23:16:18 -0300

    Hotfix

--- src/app/configure/preview/DesignPreview.tsx
+++ src/app/configure/preview/DesignPreview.tsx
@@ -121,9 +121,9 @@ const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
             </div>
 
             <div className='mt-8 flex justify-end pb-12'>
-                <Button className='px-4 sm:px-6 lg:px-8'>
-                    Check out <ArrowRight className='size-4 ml-1.5 inline' />
-                </Button>
+              <Button className='px-4 sm:px-6 lg:px-8'>
+                Check out <ArrowRight className='size-4 ml-1.5 inline' />
+              </Button>
             </div>
           </div>
         </div>
