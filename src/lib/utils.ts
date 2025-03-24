445284902b9e8a42cc7182024e4c7bef45417016
João Piologo <joaotpiologo@hotmail.com>
2025-03-24 09:15:53 -0300

    Hotfix: Update metadata

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,15 +1,51 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx'
+import type { Metadata } from 'next'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
 export const formatPrice = (price: number) => {
-  const formatter = new Intl.NumberFormat("en-US", {
+  const formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
   })
 
   return formatter.format(price)
+}
+
+export function constructMetadata({
+  title = 'CaseCobra - Custom high-quality phone cases',
+  description = 'Create custom high-quality phone cases in seconds',
+  image = '/thumbnail.png',
+  icons = '/favicon.ico',
+}: {
+  title?: string
+  description?: string
+  image?: string
+  icons?: string
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image,
+        },
+      ],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title,
+      description,
+      images: [image],
+      creator: '@jpiologo'
+    },
+    icons,
+    metadataBase: new URL('https://casecobra-taupe.vercel.app/')
+  }
 }
